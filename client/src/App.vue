98cdaebdad158adba8ab8898458bb90eb14d7693
Bman1105 <112725723+Bman1105@users.noreply.github.com>
2023-10-12 14:10:55 -0400

    vue file updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,28 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
-
 </script>
 
 <template>
   <header>
     <NavBar />
   </header>
-    
 
-    <div class="container">
-      <RouterView />
-    </div>
+  <div class="container">
+    <RouterView />
+  </div>
 
-  
 </template>
 
 <style scoped>
+  body {
+    background-color: azure;
+  }
 
-body{
-  background-color: aliceBlue;
-}
-
-.container{
-  background-color: white;
-  box-shadow: rgba(0,0,0,0.2);
-}
-
+  .container {
+    background-color: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    padding: 5px;
+    min-height: 100vh;
+  }
 </style>
